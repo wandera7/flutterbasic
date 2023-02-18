e6646e0f53ad8b6b6b1a6e9fab865d1cd1e46e6b
wandera7 <julius.wandera@student.moringaschool.com>
2023-02-18 08:59:55 +0300

    Final Reset widget

--- lib/reset.dart
+++ lib/reset.dart
@@ -1,12 +1,14 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
 class Reset extends StatelessWidget {
-  const Reset({Key key}) : super(key: key);
+  final Function resetTexts;
+  Reset(this.resetTexts);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const ElevatedButton(onPressed: null, child: Text('Reset texts'));
   }
 }
