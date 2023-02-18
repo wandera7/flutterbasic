df1a37b689026adba5a3f10b7295a86acda2a736
wandera7 <julius.wandera@student.moringaschool.com>
2023-02-18 08:41:16 +0300

    TextControl widget

---
+++ lib/textcontrol.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class TextControl extends StatelessWidget {
+  const TextControl({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
