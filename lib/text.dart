9188009ef66511f08a0e1cb40842998038f29b9b
wandera7 <julius.wandera@student.moringaschool.com>
2023-02-18 08:38:58 +0300

    TextShowing widget

--- lib/text.dart
+++ lib/text.dart
@@ -2,16 +2,19 @@ import 'package:flutter/material.dart';
 
 
 class TextShowing extends StatelessWidget {
-  const TextShowing({super.key});
+  final List<String> texts;
+  final int textIndex;
+  // ignore: prefer_const_constructors_in_immutables
+  TextShowing(this.texts,this.textIndex);
 
   @override
   Widget build(BuildContext context) {
     return Container(
               width: double.infinity,
               margin: const EdgeInsets.all(20),
-              child: const Text(
-                'My question',
-                style: TextStyle(fontSize: 27),
+              child: Text(
+                texts[textIndex],
+                style: const TextStyle(fontSize: 27),
                 textAlign: TextAlign.center,
               ),
             );
