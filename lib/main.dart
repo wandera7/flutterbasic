bd486f1ce61d59bb127f421ce4435fe3ba468d50
wandera7 <julius.wandera@student.moringaschool.com>
2023-02-18 07:44:24 +0300

    Added state in main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  //runApp();
+  runApp(MyApp());
 }
 
+
+// ignore: use_key_in_widget_constructors
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
